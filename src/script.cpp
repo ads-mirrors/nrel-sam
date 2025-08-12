598d7f0abc1cef7312e9acb28b2fd15a34d89645
sjanzou <steven@janzouconsulting.com>
2025-08-12 09:27:03 -0600

    Address show_page issue not updating navigation menu (#2097)

--- src/script.cpp
+++ src/script.cpp
@@ -585,7 +585,8 @@ void fcall_show_page(lk::invoke_t &cxt)
 	wxString page_name = cxt.arg(0).as_string();
 	Case *active_case = CurrentCase();
 	if (CaseWindow *case_window = SamApp::Window()->GetCaseWindow(active_case))
-		cxt.result().assign((case_window->SwitchToPage(page_name) ? 1.0 : 0.0));
+		cxt.result().assign((case_window->SwitchToNavigationMenu(page_name) ? 1.0 : 0.0));
+//	cxt.result().assign((case_window->SwitchToPage(page_name) ? 1.0 : 0.0));
 	else cxt.error("no active case");
 }
 
